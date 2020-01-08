1bcdcd3b384f226aa6de5f4d00e443b3c4820607
Pedro Henrique Gonzaga <53120041+ph-gonzaga@users.noreply.github.com>
2020-01-08 10:37:36 -0300

    Update sem_sucesso.test.js
    Atualização de roteiro de teste

--- zombieplus/tests/login/sem_sucesso.test.js
+++ zombieplus/tests/login/sem_sucesso.test.js
@@ -12,23 +12,41 @@ module.exports = {
     //   .assert.containsText(userInfo, "Pedro")
     //    .end();
     //},
-    'Dado que eu acesso a pagina de login':(browser) => {
+    'Senha incorreta':(browser) => {
+        let alert = ".alert-danger"
         browser
         .resizeWindow(1920,1080)
         .url("http://zombie-web:5000/login")
         .waitForElementVisible(".card-login", 3000)
-        
-    },
-    'Quando eu faço login com dados invalidos': function (browser){
-        browser
         .setValue("input[name=email]", "pedro@teste.com")
         .setValue("input[name=password]", "123")
         .click(".login-button")
-    },
-    'Então o alert devera ser apresentado': function (browser){
-        let alert = ".alert"
-        browser
         .waitForElementVisible(alert, 3000)
+        .assert.containsText(alert, 'Usuário e/ou senha inválidos')
+    },
+    'Não cadastrado': (browser) => {
+        let alert = ".alert-danger"
+        browser
+        .resizeWindow(1920,1080)
+        .url("http://zombie-web:5000/login")
+        .waitForElementVisible(".card-login", 3000)
+        .setValue("input[name=email]", "pedro404@teste.com")
+        .setValue("input[name=password]", "pwd123")
+        .click(".login-button")
+        .waitForElementVisible(alert, 3000)
+        .assert.containsText(alert, 'Usuário e/ou senha inválidos')
+    },
+    'Email não informado': (browser) => {
+        let alert = ".alert-info"
+        browser
+        .resizeWindow(1920,1080)
+        .url("http://zombie-web:5000/login")
+        .waitForElementVisible(".card-login", 3000)
+        .setValue("input[name=email]", "")
+        .setValue("input[name=password]", "pwd123")
+        .click(".login-button")
+        .waitForElementVisible(alert, 3000)
+        .assert.containsText(alert, 'Opps. Cadê o email?')
         .end();
     }
 }
